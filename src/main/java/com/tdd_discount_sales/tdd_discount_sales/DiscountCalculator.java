88a891a10f24766107d6daf793fc8ebdfd8581a0
Ana Karolina <ana.k@thoughtworks.com>
2020-01-15 20:15:32 -0300

    Initial commit

---
+++ src/main/java/com/tdd_discount_sales/tdd_discount_sales/DiscountCalculator.java
@@ -0,0 +1,7 @@
+package com.tdd_discount_sales.tdd_discount_sales;
+
+public class DiscountCalculator {
+    public double calculateDiscount(double salesValue) {
+        return salesValue;
+    }
+}
---
+++ src/test/java/com/tdd_discount_sales/tdd_discount_sales/DiscountCalculatorTest.java
@@ -0,0 +1,15 @@
+package com.tdd_discount_sales.tdd_discount_sales;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DiscountCalculatorTest {
+
+    DiscountCalculator discountCalculator = new DiscountCalculator();
+
+    @Test
+    public void notGiveDiscountToSalesValueIs499(){
+
+    }
+}
