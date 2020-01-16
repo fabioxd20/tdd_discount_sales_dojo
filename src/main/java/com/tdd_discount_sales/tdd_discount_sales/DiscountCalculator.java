a3c3d36c90bf32c64e36b6be4abfd87419471f85
Fabio Lopes <fabioxd.20@gmail.com>
2020-01-16 15:12:26 -0300

    Create discount calculator by: Fabio and Yuri

--- src/main/java/com/tdd_discount_sales/tdd_discount_sales/DiscountCalculator.java
+++ src/main/java/com/tdd_discount_sales/tdd_discount_sales/DiscountCalculator.java
@@ -1,4 +1,25 @@
 package com.tdd_discount_sales.tdd_discount_sales;
 
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.Map;
+
 public class DiscountCalculator {
+    public double calculateFinalValue(double salesValue) {
+        return salesValue - this.calculateDiscountValue(salesValue);
+    }
+
+    public double calculateDiscountValue(double salesValue) {
+
+        if(salesValue >= 1000) {
+            return salesValue * .2;
+        }
+        else if (salesValue >= 500) {
+            return salesValue * .1;
+        }
+        else {
+            return salesValue * .05;
+        }
+    }
 }
+
--- src/test/java/com/tdd_discount_sales/tdd_discount_sales/DiscountCalculatorTest.java
+++ src/test/java/com/tdd_discount_sales/tdd_discount_sales/DiscountCalculatorTest.java
@@ -9,7 +9,34 @@ class DiscountCalculatorTest {
     DiscountCalculator discountCalculator = new DiscountCalculator();
 
     @Test
-    public void notGiveDiscountToSalesValueIs499(){
+    public void calculateDiscountWhenBiggerThanOrEquals500(){
+        double salesValue = 500.00;
+        double finalValue = 450.00;
 
+        assertEquals(finalValue, discountCalculator.calculateFinalValue(salesValue));
+    }
+
+    @Test
+    public void calculateDiscountWhenBiggerThanOrEquals1000(){
+        double salesValue = 1000.00;
+        double finalValue = 800.00;
+
+        assertEquals(finalValue, discountCalculator.calculateFinalValue(salesValue));
+    }
+
+    @Test
+    public void calculateDiscountWhenValueIsOverOrEquals1000(){
+        double salesValue = 1000.00;
+        double discountValue = 200.00;
+
+        assertEquals(discountValue, discountCalculator.calculateDiscountValue(salesValue));
+    }
+
+    @Test
+    public void calculateFinalValueWhenValueIsUnder500(){
+        double salesValue = 400.00;
+        double finalValue = 380.00;
+
+        assertEquals(finalValue, discountCalculator.calculateFinalValue(salesValue));
     }
 }
