c14301c69d66ebe5f752fb37fe25bc48fdd6c05d
lamarianaramos <mariana.r1@thoughtwortks.com>
2020-01-16 11:58:45 -0300

    Remove implemented method

--- src/main/java/com/tdd_discount_sales/tdd_discount_sales/DiscountCalculator.java
+++ src/main/java/com/tdd_discount_sales/tdd_discount_sales/DiscountCalculator.java
@@ -1,7 +1,4 @@
 package com.tdd_discount_sales.tdd_discount_sales;
 
 public class DiscountCalculator {
-    public double calculateDiscount(double salesValue) {
-        return salesValue;
-    }
 }
